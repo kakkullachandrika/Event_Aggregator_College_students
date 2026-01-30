5b2435846d42015e640e1ee4e853c838be538028
Keerthi Shamanthula <shamanthulakeerthi46@gmail.com>
2026-01-30 20:33:22 +0530

    added certifications page

--- event_aggregator_students/landing/views.py
+++ event_aggregator_students/landing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
+from .models import Certification
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to the home page")
@@ -15,4 +16,15 @@ def placements(request):
     return render(request, 'placements.html')
 
 def certifications(request):
-    return render(request, 'certifications.html')
+    if request.method=='POST':
+        event_name =request.POST.get('event_name ')
+        event_category=request.POST.get('event_category')
+        date_and_time=request.POST.get('date_and_time')
+        venue=request.POST.get('venue')
+        organizer=request.POST.get('organizer')
+        registration_link=request.POST.get('registration_link')
+        cer=Certification(event_name=event_name, event_category= event_category, date_and_time= date_and_time,venue=venue,organizer=organizer,registration_link=registration_link)
+        cer.save()
+    c=Certification.objects.all().order_by('-id')
+    return render(request , 'certifications.html',{'certification':c})
+   
