2dfcadc3e30bd83d24b23b8d0f4b7c6457575ec4
kakkullachandrika <kakkullachandrika@gmail.com>
2026-01-31 01:34:31 +0530

    Update urls.py

--- event_aggregator_students/landing/urls.py
+++ event_aggregator_students/landing/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('clubs/', views.clubs, name='clubs'),
     path('placements/', views.placements, name='placements'),
     path('certifications/', views.certifications, name='certifications'),
+    path('add-event/', views.add_event, name='add_event'),
+    path("social/facebook/", views.facebook_page, name="facebook"),
+    path("social/instagram/", views.instagram_page, name="instagram"),
+    path("social/twitter/", views.twitter_page, name="twitter"),
+    path("social/whatsapp/", views.whatsapp_page, name="whatsapp"),
+
 ]
